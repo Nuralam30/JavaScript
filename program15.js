fb32cce77faa4f125cfea7a05399b0eca42048d4
Nur Alam <56593129+Nuralam30@users.noreply.github.com>
2020-05-12 18:04:17 +0600

    mathObject in js

---
+++ program15.js
@@ -0,0 +1,25 @@
+var x = Math.round(9.1416); //round() mathod
+document.write(x +"<br/>");
+document.write(Math.sqrt(x) +"<br/>"); //sqrt() Mathod
+
+var x = Math.round(9.7416);
+document.write(x +"<br/>");
+
+var x = Math.abs(-12); //abs() mathod
+document.write(x +"<br/>");
+
+document.write(Math.sin(2) +"<br/>"); //sin() method
+
+document.write(Math.pow(4,2) +"<br/>"); //pow() method
+
+document.write(Math.ceil(3.1416) +"<br/>"); //ceil()method
+
+document.write(Math.floor(3.1416) +"<br/>"); //floor method
+
+document.write(Math.max(4,2, 10, -5) +"<br/>");
+
+document.write(Math.min(4,2, 10, -5) +"<br/>");
+
+document.write(Math.random() +"<br/>"); //random() method
+
+document.write(Math.floor(Math.random()*6) +"<br/>"); //0-5 random number
