2ceddbf9c2c0c7eb5f74ced702cea1461e13c69d
Nur Alam <56593129+Nuralam30@users.noreply.github.com>
2020-05-13 19:21:57 +0600

    html events by js

---
+++ program18.js
@@ -0,0 +1,9 @@
+//getElementById method
+document.getElementById("bird").innerHTML = "The bird flew away.";
+
+//getElementByTagName method
+document.getElementsByTagName("h3")[1].innerHTML = "She has smiley face";
+
+
+//getElementByClass method
+document.getElementsByClassName("name")[1].innerHTML = "I am learning web development";
